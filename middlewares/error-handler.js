3692c295d7abbf267467c60d90c675819ebfb16c
alexeybersh <balexworks@gmail.com>
2023-12-04 15:01:40 +0300

    ПР14

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-unused-vars */
-const { ERROR_SERVER } = require('../utils/errorsStatus')
+const { ERROR_SERVER } = require('../utils/errorsStatus');
 
 module.exports.errorHandler = (err, req, res, next) => {
-  const { statusCode = ERROR_SERVER } = err.statusCode;
+  const { statusCode = ERROR_SERVER, message } = err;
 
-  res.status(statusCode).send({ message: statusCode === ERROR_SERVER ? 'На сервере произошла ошибка' : err.message });
+  res.status(statusCode).send({ message: statusCode === ERROR_SERVER ? 'На сервере произошла ошибка' : message });
 };
